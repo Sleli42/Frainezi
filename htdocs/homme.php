e4f3cfeb8a36f6ffc404910f08dfc320f6450b05
Sleli42 <lubaujar@student.42.fr>
2015-06-26 05:49:02 +0200

    pull

--- htdocs/homme.php
+++ htdocs/homme.php
@@ -1,6 +1,7 @@
 <?PHP
 
 require_once('includes/header.php');
-require_once('includes/acceuil.php');
+require_once('includes/homme.php');
 require_once('includes/footer.php');
+
 ?>
--- htdocs/includes/contact.php
+++ htdocs/includes/contact.php
@@ -3,7 +3,7 @@
 <head><link rel="stylesheet" type="text/css" href="../style/bootstrap.css"></head>
 
 <body><center>
-<div class="main-men">
+<div class="main-contact">
 
 		</div>
 </center></body>
