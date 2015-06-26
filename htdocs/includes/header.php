9ef991d3ea9fb36172755ab90be83ea312a59c5e
Sleli42 <lubaujar@student.42.fr>
2015-06-26 05:30:02 +0200

    o'

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -9,10 +9,10 @@
 		<h1>FRAINEZI</h1>
 		<ul class="menu">
 		<li><a href="index.php">Acceuil</a></li>
-			<li><a href="shop.php">Shop</a></li>
-			<li><a href="article.php">Articles</a></li>
-			<li><a href="inscription.php">Inscription</a></li>
-			<li><a href="connexion.php">Connexion</a></li>
+			<li><a href="homme.php">Homme</a></li>
+			<li><a href="femme.php">Femme</a></li>
+			<li><a href="panier.php">Panier</a></li>
+			<li><a href="contact.php">contact</a></li>
 		</ul></center>
 	</header>
 
