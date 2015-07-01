255aad3b311d601c2f386fbfac9f47413e01e001
Sleli42 <lubaujar@student.42.fr>
2015-07-01 04:38:14 +0200

    04:38 last push

--- htdocs/includes/femme.php
+++ htdocs/includes/femme.php
@@ -3,8 +3,28 @@
 <head><link rel="stylesheet" type="text/css" href="../style/femme.css"></head>
 
 <body><center>
-<div class="main" style="background-color: #eab4fd;">
-
-		</div>
+	<div class="main" style="background-color: #f3cdfb;">
+		<span id="sl_play" class="sl_command"></span>
+		<span id="sl_pause" class="sl_command"></span>
+		<span id="sl_i1" class="sl_command sl_i"></span>
+		<span id="sl_i2" class="sl_command sl_i"></span>
+		<section id="slideshow">
+			<a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+			<a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+			<div id="container">
+				<div class="slider">
+					<figure>
+						<img src="../img/photoshop/decoupe_tshirt_rose.jpg" alt="" width="640px" height="420px" />
+						<figcaption>T-shirt Rose</figcaption>
+					</figure>
+					<figure>
+						<img src="../img/photoshop/decoupe_tshirt_noir.jpg" alt="" width="640px" height="420px" />
+						<figcaption>T-shirt<em>Noir</em></figcaption>
+					</figure>
+				</div>
+			</div>
+			<span id="timeline"></span>
+		</section>
+	</div>
 </center></body>
 </html>
--- htdocs/includes/homme.php
+++ htdocs/includes/homme.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head><link rel="stylesheet" type="text/css" href="../style/homme.css"></head>
-
-<body><center>
-<div class="main" style="background-color: #c7e9ff;">
-
+<body>
+	<center>
+		<div class="main" style="background-color: #c7e9ff;">
+			<div class="slideshow">
+				<ul>
+					<li><img height="640px" width="640px" align="center" src="../img/photoshop/decoupe_joker.jpg"></li>
+					<li><img height="640px" width="640px" align="center" src="../img/photoshop/decoupe_tshirt_noir.jpg"></li>
+					<li><img height="640px" width="640px" align="center" src="../img/photoshop/decoupe_tshirt_rose.jpg"></li>
+				</ul>
+			</div>
 		</div>
-</center></body>
+	</center>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js">
+	</script>
+
+	<script type="text/javascript">
+  		(function($){
+  setInterval(function(){
+    $(".slideshow ul li:first-child").animate({"margin-left": -650}, 800, function(){
+        $(this).css("margin-left",0).appendTo(".slideshow ul");
+    });
+}, 3500);
+})(jQuery);
+	</script>
+</body>
 </html>
