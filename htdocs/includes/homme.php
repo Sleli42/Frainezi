51c3442709db1b01f7deb4b7cd04ee5f3f0dcb1f
Sleli42 <lubaujar@student.42.fr>
2015-07-03 05:16:51 +0200

    mise en page homme

--- htdocs/includes/homme.php
+++ htdocs/includes/homme.php
@@ -4,6 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="../style/homme.css">
 	<link rel="stylesheet" href="flexslider.css" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 	<script src="jquery.flexslider.js"></script>
 	<script type="text/javascript" charset="utf-8">
@@ -14,43 +15,79 @@
 </head>
 <body>
 	<center>
-		<div class="main" style="background-color: #c7e9ff;">
+		<div class="main">
 			<div id="sectionProd">
-				<section >
+				<section>
+				<div class="areaProd">
 					<article class="newProd">
-					<div class="flexslider">
-					 <ul class="slides">
-					   	  <li>
-						      <img height="300px" width="300px" src="../img/tsblack.jpg" />
-						    </li>
-						    <li>
-						      <img height="300px" width="300px" src="../img/tsred.jpg" />
-						    </li>
-				 	 </ul>
-					</div>
-					</article> <BR />
-					<article class="newProd">
-						<img height="300px" width="300px" src="../img/tsjoker.jpg">
-					</article> <BR />
-					<article class="newProd">
+						<div class="flexslider">
+							 <ul class="slides">
+							   	  <li>
+								      <img src="../img/photoshop/ts-noir-300px300.jpg"/>
+								    </li>
+								    <li>
+								      <img src="../img/photoshop/ts-red-300px300.jpg"/>
+								    </li>
+						 	 </ul>
+						</div>
+						<div class="infoProd"></div>
 					</article>
+				</div>
+				<div class="line"></div>
+				<BR />
+					<div class="areaProd">
+						<article class="newProd">
+							<div class="flexslider">
+							 <ul class="slides">
+							   	  <li>
+								      <img src="../img/photoshop/ts_joker-300px300.jpg">
+								    </li>
+								    <li>
+								      <img src="../img/photoshop/ts_joker-300px300.jpg">
+								    </li>
+						 	 </ul>
+						</div>
+							<div class="infoProd"></div>
+						</article>
+					</div>
+					<div class="line"></div>
+					<BR />
+					<div class="areaProd">
+						<article class="newProd">
+							<div class="flexslider">
+							 <ul class="slides">
+							   	  <li>
+								      <img src="../img/photoshop/ts_joker-300px300.jpg">
+								    </li>
+								    <li>
+								      <img src="../img/photoshop/ts_joker-300px300.jpg">
+								    </li>
+						 	 </ul>
+						</div>
+							<div class="infoProd"></div>
+						</article>
+					</div>
+					<div class="line"></div>
 				</section>
 			</div>
-			<div class="infoProd">
-				<h1>T-SHIRT FRAINEZI HOMME SARAJEVO</h1>
+<!-- 			<div class="infoProd">
+				<h1>T-SHIRT FRAINEZI SARAJEVO</h1>
 				<p class="description">petite description du produit ...</p>
 				<p class="prix">10<font class="centimes">€90</font></p>
+				<a class="moreInfo" href="">+ d'infos</a>
 			</div>
 			<div class="infoProd">
-				<h1>T-SHIRT FRAINEZI HOMME JOKER</h1>
+				<h1>T-SHIRT FRAINEZI JOKER</h1>
 				<p class="description">petite description du produit ...</p>
 				<p class="prix">10<font class="centimes">€99</font></p>
+				<a class="moreInfo" href="">+ d'infos</a>
 			</div>
 			<div class="infoProd">
-				<h1>T-SHIRT FRAINEZI HOMME OTHER</h1>
+				<h1>T-SHIRT FRAINEZI OTHER</h1>
 				<p class="description">petite description du produit ...</p>
 				<p class="prix">9<font class="centimes">€99</font></p>
-			</div>
+				<a class="moreInfo" href="">+ d'infos</a>
+			</div> -->
 		</div>
 	</center>
 </body>
